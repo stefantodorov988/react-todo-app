80f8c07b23107988a45f11cfb288eac48849ef5f
Stefan Todorov <stodorov988@gmail.com>
2023-06-07 14:59:55 +0300

    Added withTodos hoc and formatted files

--- src/helpers/localstorage.helper.js
+++ src/helpers/localstorage.helper.js
@@ -1,20 +1,20 @@
 export const loadState = () => {
-	try {
-	const serializeState = localStorage.getItem('todoList');
-	if (serializeState === null) {
-		return [];
-	}
-	return JSON.parse(serializeState);
-	} catch (err) {
-	return [];
-	}
+  try {
+    const serializeState = localStorage.getItem("todoList");
+    if (serializeState === null) {
+      return [];
+    }
+    return JSON.parse(serializeState);
+  } catch (err) {
+    return [];
+  }
 };
 
 export const saveState = (state) => {
-	try {
-	const serializeState = JSON.stringify(state);
-	localStorage.setItem('todoList', serializeState);
-	} catch(err) {
-		console.log(err);
-	}
+  try {
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem("todoList", serializeState);
+  } catch (err) {
+    console.log(err);
+  }
 };
---
+++ src/hocs/withTodos.js
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { update } from "../store/reducers/todo.reducer";
+
+export const withTodos = Component => props => {
+    const todos = useSelector((state) => state.todo);
+    const dispatch = useDispatch();
+
+    const handleUpdate = useCallback((value) => {
+        dispatch(update(value));
+      },[]);
+
+    return <Component {...props} todos={todos} handleUpdate={handleUpdate}></Component>
+}
